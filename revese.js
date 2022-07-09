753847fcf9d2ac0db1668a2ab14891c27a335117
shivam-mrk <108858447+shivam-mrk@users.noreply.github.com>
2022-07-09 23:06:40 +0530

    reverse,clone,clear program A
    reverse,clone,clear program A

---
+++ revese.js
@@ -0,0 +1,18 @@
+function c(){
+	var a= document.getElementById("id1").value;
+	var b= document.getElementById("id2").value;
+	document.getElementById("id2").value=a;
+	document.getElementById("id1").value=b;
+document.getElementById("id3").value=a;
+}
+
+function d(){
+	console.log("shivam")
+	var ab=document.getElementById("id5").value;
+	document.getElementById("id6").value=ab;
+}
+
+function e(){
+	console.log("working")
+	document.getElementById("id7").value="";
+}
